7122711dd4f9e4639cf6e6ec43a1199ba06dd799
md sujon <mdsujonfkyour@gmail.com>
2026-09-21 09:13:39 +0600

    Add Tailwind CSS configuration file

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6B00',
+        secondary: '#1F2937',
+        dark: '#111827',
+        accent: '#00D9FF',
+      }
+    },
+  },
+  plugins: [],
+}
